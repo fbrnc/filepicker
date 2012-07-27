3b01662539e32cd33aec93abb8591c3816e84de4
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2012-07-26 23:35:03 -0700

    Configurable services

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "fal".
+# Extension Manager/Repository config file for ext "filepicker".
 #
-# Auto generated 16-10-2010 16:38
+# Auto generated 26-07-2012 23:18
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'misc',
 	'author' => 'Fabrizio Branca',
 	'author_email' => 'typo3@fabrizio-branca.de',
-	'shy' => '0',
+	'shy' => 0,
 	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"9e69";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"ef52";s:14:"ext_tables.sql";s:4:"6cce";s:29:"icon_tx_fal_contentimages.gif";s:4:"475a";s:25:"icon_tx_fal_sys_files.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"2c50";s:7:"tca.php";s:4:"72f8";s:19:"doc/wizard_form.dat";s:4:"6704";s:20:"doc/wizard_form.html";s:4:"ea93";}',
+	'_md5_values_when_last_written' => 'a:6:{s:16:"ext_autoload.php";s:4:"ad4c";s:21:"ext_conf_template.txt";s:4:"c83a";s:12:"ext_icon.gif";s:4:"0547";s:17:"ext_localconf.php";s:4:"be18";s:55:"classes/hooks/class.tx_filepicker_hooks_dbfileicons.php";s:4:"a651";s:58:"classes/hooks/class.tx_filepicker_hooks_processdatamap.php";s:4:"98d0";}',
 	'suggests' => array(
 	),
 );
